da6820655b3f8b6bfb2906cbd34cae23ca4e0f70
nick <nick@mountainairweb.com>
2024-12-05 21:57:01 -0800

    synced repo

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <div class="c-pre-footer">
     <div class="o-wrapper-wide">
-    <p>Medical | Counselling | Dietitian | Pelvic Floor Physiotherapy | Physiotherapy | Massage Therapy </p>
+    <p><a href="/services/medical-services/">Medical</a> | <a href="/services/counselling-services/">Counselling</a> | <a href="/services/dietitian-services/">Dietitian</a> | <a href="/services/physiotherapy-pelvic-floor/">Pelvic Floor Physiotherapy</a> | <a href="/services/physiotherapy-pelvic-floor/">Physiotherapy</a> | <a href="/services/massage-therapy/">Massage Therapy</a> </p>
 <p class="c-pre-footer-tag">Healthcare for all women, all trans, and all non-binary individuals.</p>
     </div>
   </div>
@@ -8,19 +8,25 @@
     <div class="c-footer-main">
       <div class="o-wrapper-wide">
       <div class="grid-x">
-          <div class="cell  medium-4">
+          <div class="cell  medium-6 large-3 c-footer-cell-one">
             <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
               <?php dynamic_sidebar( 'footer-1' ); ?>
             <?php endif; ?>
-          </div>
-          <div class="cell  medium-5">
+          </div> 
+          <div class="cell  medium-6 large-3 c-footer-cell-two">
           <?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
             <?php dynamic_sidebar( 'footer-2' ); ?>
           <?php endif; ?>
           </div>
-          <div class="cell  medium-3">
+          <div class="cell  medium-6 large-3">
           <?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
             <?php dynamic_sidebar( 'footer-3' ); ?>
+          <?php endif; ?>
+          </div>
+
+          <div class="cell  medium-6 large-3">
+          <?php if ( is_active_sidebar( 'footer-4' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-4' ); ?>
           <?php endif; ?>
           </div>
      
